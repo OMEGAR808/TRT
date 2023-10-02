8b93526946c8343f919bcca3988932819f53f489
nayhtetlin007 <143700605+OMEGAR808@users.noreply.github.com>
2023-10-02 19:37:37 +0630

    Update TRT.py

--- TRT.py
+++ TRT.py
@@ -570,7 +570,7 @@ def api2(ids,names,passlist):
                         en = random.choice(['en_US','en_GB'])
                         cph = random.choice(['CPH1979','CPH1983','CPH1987','CPH2005','CPH2009','CPH2015','CPH2059','CPH2061','CPH2065','CPH2069','CPH2071','CPH2073','CPH2077','CPH2091','CPH2095','CPH2099','CPH2137','CPH2139','CPH2145','CPH2161','CPH2185','CPH2201','CPH2209','CPH1801','CPH1803','CPH1805','CPH1809','CPH1827','CPH1837','CPH1851','CPH1853'])
                         network = random.choice(['Zong','null','Marshmallow','Telekom China'])
-                        ua = 'Davik/2.1.0 (Linux; U; Android 13; Redmi Note 12 Turbo Build/TP1A.220905.001) [FBAN/FB4A;FBAV/398.0.0.33.130;FBBV/35416168;FBDM/{density=1.5,width=1280,height=1440};FBLC/en_GB;FBRV/94914047;FBCR/Nepal_Telecom;FBMF/Redmi;FBBD/Redmi;FBPN/com.facebook.katana;FBDV/Redmi Note 12 Turbo;FBSV/6.5.1;FBOP/1;FBCA/armeabi-v7a:armeabi;]'
+                        ua = 'Davik/2.1.0 (Linux; U; Android 8.0.1; Redmi Note 8 Build/TP1A.220905.001) [FBAN/FB4A;FBAV/398.0.0.33.130;FBBV/35416168;FBDM/{density=1.5,width=1280,height=1440};FBLC/en_GB;FBRV/94914047;FBCR/Nepal_Telecom;FBMF/Redmi;FBBD/Redmi;FBPN/com.facebook.katana;FBDV/Redmi Note 8;FBSV/6.5.1;FBOP/1;FBCA/armeabi-v7a:armeabi;]'
                         random_seed = random.Random()
                         adid = str(''.join(random_seed.choices(string.hexdigits, k=16)))
                         device_id = str(uuid.uuid4())
