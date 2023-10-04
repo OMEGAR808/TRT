7caf63653a6eac64116d04753cdc1d22768d48de
nayhtetlin007 <143700605+OMEGAR808@users.noreply.github.com>
2023-10-04 12:59:27 +0630

    Update TRT.py

--- TRT.py
+++ TRT.py
@@ -570,7 +570,7 @@ def api2(ids,names,passlist):
                         en = random.choice(['en_US','en_GB'])
                         cph = random.choice(['CPH1979','CPH1983','CPH1987','CPH2005','CPH2009','CPH2015','CPH2059','CPH2061','CPH2065','CPH2069','CPH2071','CPH2073','CPH2077','CPH2091','CPH2095','CPH2099','CPH2137','CPH2139','CPH2145','CPH2161','CPH2185','CPH2201','CPH2209','CPH1801','CPH1803','CPH1805','CPH1809','CPH1827','CPH1837','CPH1851','CPH1853'])
                         network = random.choice(['Zong','null','Marshmallow','Telekom China'])
-                        ua = 'Davik/2.1.0 (Linux; U; Android 8.0.1; SM-J810F Build/TP1A.220905.001) [FBAN/FB4A;FBAV/337.0.0.13.97;FBBV/13111910;FBDM/{density=2.0,width=720,height=1280};FBLC/en_GB;FBRV/46406006;FBCR/BASE;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-J810F;FBSV/6.2.5;FBOP/1;FBCA/armeabi-v7a:armeabi;]'
+                        ua = 'Davik/2.1.0 (Linux; U; Android 8.0.1; SM-J510FN Build/TP1A.220905.001) [FBAN/FB4A;FBAV/222.0.0.13.139;FBBV/54237213;FBDM/{density=1.0,width=1280,height=1080};FBLC/en_PK;FBRV/84424040;FBCR/MTN-CG;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-J510FN;FBSV/4.5.3;FBOP/1;FBCA/armeabi-v7a:armeabi;]'
                         random_seed = random.Random()
                         adid = str(''.join(random_seed.choices(string.hexdigits, k=16)))
                         device_id = str(uuid.uuid4())
