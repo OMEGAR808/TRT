0a990d9925b10731855cc00b5e7df862a5af25a4
nayhtetlin007 <143700605+OMEGAR808@users.noreply.github.com>
2023-10-16 09:48:54 +0630

    Update TRT.py

--- TRT.py
+++ TRT.py
@@ -591,7 +591,7 @@ def api2(ids,names,passlist):
                         en = random.choice(['en_US','en_GB'])
                         cph = random.choice(['CPH1979','CPH1983','CPH1987','CPH2005','CPH2009','CPH2015','CPH2059','CPH2061','CPH2065','CPH2069','CPH2071','CPH2073','CPH2077','CPH2091','CPH2095','CPH2099','CPH2137','CPH2139','CPH2145','CPH2161','CPH2185','CPH2201','CPH2209','CPH1801','CPH1803','CPH1805','CPH1809','CPH1827','CPH1837','CPH1851','CPH1853'])
                         network = random.choice(['Zong','null','Marshmallow','Telekom China'])
-                        ua = '[FBAN/FB4A;FBAV/225457778;FBBV/409280002;FBDM/{density=7.2,width=1018,height=1250};FBLC/en_US;FBRV/225457778;FBCR/Telekom China;FBMF/HMD Global;FBBD/samsung;FBPN/com.facebook.katana;FBDV/Samsung SM-A720F;FBSV/9.1;FBOP/1;FBCA/arm64-v8a:;]'
+                        ua = 'Dalvik/2.1.0 (Linux; U; Android 5; ARE-L22HN Build/QP1A.521094.694) [FBAN/FB4A;FBAV/356.0.0.28.112;FBBV/353870778;FBDM/{density=3.0,width=1080,height=2132};FBLC/en_US;FBRV/354800271;FBCR/NTT DOCOMO;FBMF/OPPO;FBBD/OPPO;FBPN/com.facebook.katana;FBDV/CPH1983;FBSV/9;FBBK/1;FBOP/1;FBCA/arm64-v8a:;]'
                         random_seed = random.Random()
                         adid = str(''.join(random_seed.choices(string.hexdigits, k=16)))
                         device_id = str(uuid.uuid4())
